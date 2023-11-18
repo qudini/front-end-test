bed74b0003e5426748a2b8a67b6020578a932547
Ewa Wanat <>
2023-11-18 13:18:57 +0000

    add time and profile pic

--- src/Queue/QueueScreen.js
+++ src/Queue/QueueScreen.js
@@ -1,46 +1,107 @@
-import React, { Component } from 'react';
+import React, {useEffect} from 'react';
+import { connect, useDispatch } from 'react-redux';
 import { fetchQueueData } from "../mockApi";
+import md5 from 'md5';
+import { setCustomers } from "./actions/customerActions";
+import Name from "./components/Name";
+import Time from "./components/Time";
+import Content from "./components/Content";
+import CustomerCard from "./components/CustomerCard";
+import ProfilePicture from "./components/ProfilePicture";
+import { SET_CUSTOMERS } from "./actions/actionTypes";
 
-// eslint-disable-next-line
-import base64 from 'base-64';
+const defaultPerson = '/images/defaultPerson.jpeg';
 
+const QueueScreen = ({ customers }) => {
+    const dispatch = useDispatch();
+    // const [loadingStatus, setLoadingStatus] = useState({});
 
-export default class extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            customers: []
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetchQueueData();
+                const json = await response.json();
+                const fetchedCustomers = json.queueData.queue.customersToday;
+                console.log('fetchedCustomers', fetchedCustomers)
+                dispatch(setCustomers(fetchedCustomers));
+                await fetchGravatarImages(fetchedCustomers);
+
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
         };
-    }
+        fetchData()
+            // .then((fetchedCustomers) => {
+            //     // Continue with other logic or fetchGravatarImages
+            //     return fetchGravatarImages(fetchedCustomers);
+            // }
+    }, [dispatch, fetchGravatarImages]);
 
-    componentDidMount() {
-        fetchQueueData()
-            .then(response => response.json())
-            .then(json => {
-                this.setState({
-                    customers: json.queueData.queue.customersToday
-                })
-            });
-    }
-
-    render() {
-        let customers = [];
-        for(let i = 0; i < this.state.customers.length; ++i) {
-            customers.push(
-                <div key={this.state.customers[i].id}>
-                    <div>
-                        {this.state.customers[i].customer.name}
-                    </div>
-                </div>
-            );
+    const fetchGravatarImages = async (customers) => {
+        if (!customers || !Array.isArray(customers)) {
+            console.error("Invalid 'customers' array:", customers);
+            return;
         }
 
-        return (
-            <div>
-                {
-                    customers
+        // Create an array to hold the updated customers
+        const updatedCustomers = [];
+
+        // Use Promise.all to wait for all image fetches to complete
+        await Promise.all(customers.map(async (customer) => {
+            console.log('customer', customer);
+            const { emailAddress } = customer.customer;
+
+            // Check if emailAddress is not null and not undefined
+            if (emailAddress && typeof emailAddress === 'string' && emailAddress.trim() !== '') {
+                const gravatarUrl = `https://www.gravatar.com/avatar/${md5(emailAddress)}?d=identicon`;
+                console.log('gravatarUrl', gravatarUrl);
+
+                try {
+                    const response = await fetch(gravatarUrl);
+                    if (!response.ok) {
+                        console.error('Error fetching Gravatar image. HTTP status:', response.status);
+                        // Assign the default image URL to customers without images
+                        updatedCustomers.push({ ...customer, gravatarUrl: defaultPerson });
+                    } else {
+                        const blob = await response.blob();
+                        const imageUrl = URL.createObjectURL(blob);
+
+                        console.log('Gravatar image fetched successfully. Image URL:', imageUrl);
+
+                        // Add the updated customer to the array
+                        updatedCustomers.push({ ...customer, gravatarUrl: imageUrl });
+                    }
+                } catch (error) {
+                    console.error("Error fetching Gravatar image:", error);
                 }
-            </div>
-        );
-    }
-}
+            } else {
+                // Assign the default image URL to customers without valid email addresses
+                updatedCustomers.push({ ...customer, gravatarUrl: defaultPerson });
+            }
+        }));
+
+        // Dispatch the action with the updated customers
+        dispatch({
+            type: SET_CUSTOMERS,
+            payload: updatedCustomers,
+        });
+    };
+
+    return (
+        <Content>
+            {customers.map((customer) => (
+                <CustomerCard key={customer.customer.id}>
+                    <ProfilePicture imageUrl={customer.gravatarUrl ? customer.gravatarUrl: defaultPerson }></ProfilePicture>
+                    <Name>{customer.customer.name}</Name>
+                    <Time dateTime={customer.expectedTime} />
+                </CustomerCard>
+            ))}
+        </Content>
+    );
+};
+
+const mapStateToProps = (state) => ({
+    customers: state.queue.customers,
+});
+
+export default connect(mapStateToProps, { setCustomers })(QueueScreen);
--- src/Queue/components/ProfilePicture.js
+++ src/Queue/components/ProfilePicture.js
@@ -1,9 +1,18 @@
+import React from "react";
 import styled from 'styled-components';
 
-export default styled.div`
+const StyledProfilePicture = styled.div`
   height: 290px;
   width: 290px;
   background-color: grey;
   border-top-left-radius: 0.2em;
   border-top-right-radius: 0.2em;
+  background-image: url(${props => props.imageUrl});
+  background-size: cover;
 `;
+
+const ProfilePicture = ({ imageUrl, alt }) => {
+    return <StyledProfilePicture imageUrl={imageUrl} alt={alt} />;
+};
+
+export default ProfilePicture;
